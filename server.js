e39b9011f3c52ddad8bb0f0040bf3c08e4d63567
matias <msclavi8@gmail.com>
2019-01-14 20:34:16 -0300

    Heroku configuration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 // Lets you say wich folder contains 'partials' (reusable components)
@@ -53,6 +54,6 @@ app.get('/bad', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('Server is up listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up listening on port ${port}`);
 });
